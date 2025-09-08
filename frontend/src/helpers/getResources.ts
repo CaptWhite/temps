20a5e214359579948b01ae968fe5f63ed9ed0e8a
Your Name <you@example.com>
2025-09-09 00:44:08 +0200

    Star calculation

--- frontend/src/helpers/getResources.ts
+++ frontend/src/helpers/getResources.ts
@@ -29,7 +29,7 @@ export const getResources = async (
       formData.append("date", date);
       console.log( import.meta )
       // const response = await axios.post<GetResourcesResponse>(`${ import.meta.env.VITE_API_URL }/upload/`, formData, {
-      const response = await axios.post<GetResourcesResponse>(`http://localhost:8000/upload/`, formData, {  
+      const response = await axios.post<GetResourcesResponse>(`http://localhost:8001/upload/`, formData, {  
         headers: {
           'Content-Type': 'multipart/form-data'
         },
